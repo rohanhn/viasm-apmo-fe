1b7ecaef123eadc7fb8333eb3c306fc1d9e0c258
huongduongdev <huong.duong@trangnguyen.edu.vn>
2026-03-04 09:41:18 +0700

    update: change env

--- envs.js
+++ envs.js
@@ -1,26 +1,11 @@
 module.exports = {
   prod: {
-    NEXT_PUBLIC_API_URL: 'https://vnmf.edu.vn',
-    NEXT_PUBLIC_API_URL_V2: 'https://tnmath.edu.vn/thi-toan',
-    NEXT_PUBLIC_GA4_TRACKING_ID1: 'G-PB3YYEQH88',
-    NEXT_PUBLIC_CLARITY: 'sj6ljamm6n',
-    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: '6Leow6QnAAAAADzSqSp5muwszV8MHhHtqRpGyeTF',
-    NEXT_PUBLIC_API_NOIBO: 'https://noibo-api.trangnguyen.edu.vn/api/v1',
+    NEXT_PUBLIC_API_URL: 'https://viasm-dev.trangnguyen.edu.vn',
   },
   dev: {
-    NEXT_PUBLIC_API_URL: 'https://dev.vnmf.edu.vn',
-    NEXT_PUBLIC_API_URL_V2: 'https://tnmath.edu.vn/thi-toan',
-    NEXT_PUBLIC_GA4_TRACKING_ID1: 'G-PB3YYEQH88',
-    NEXT_PUBLIC_CLARITY: 'sj6ljamm6n',
-    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: '6Leow6QnAAAAADzSqSp5muwszV8MHhHtqRpGyeTF',
-    NEXT_PUBLIC_API_NOIBO: 'https://noibo-api-dev.trangnguyen.edu.vn/api/v1',
+    NEXT_PUBLIC_API_URL: 'https://viasm-dev.trangnguyen.edu.vn',
   },
   staging: {
-    NEXT_PUBLIC_API_URL: 'https://dev.vnmf.edu.vn',
-    NEXT_PUBLIC_API_URL_V2: 'https://tnmath.edu.vn/thi-toan',
-    NEXT_PUBLIC_GA4_TRACKING_ID1: 'G-PB3YYEQH88',
-    NEXT_PUBLIC_CLARITY: 'sj6ljamm6n',
-    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: '6Leow6QnAAAAADzSqSp5muwszV8MHhHtqRpGyeTF',
-    NEXT_PUBLIC_API_NOIBO: 'https://noibo-api-stage.trangnguyen.edu.vn/api/v1',
+    NEXT_PUBLIC_API_URL: 'https://viasm-dev.trangnguyen.edu.vn',
   },
 };
